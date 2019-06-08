30b40101aa60645dead21ad47c336b85bb779abb
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-06-08 13:38:59 -0700

    Recipe search functionality

--- js/recipe.js
+++ js/recipe.js
@@ -2,10 +2,19 @@ var apiKey = "2fecaffd34d74567cc8225f296bcbec1";
 var appID = "5ae54cff";
 var dishSearch = "";
 
-$.ajax({
-    url: "https://api.edamam.com/search?q=" + dishSearch + "&app_id=" + appID + "&app_key=" + apiKey,
-    method: "GET",
-}).then(response => {
+$("#search-button").on("click", function(event){
+    
+    event.preventDefault();
+
+    dishSearch = $("#search-bar").val();
+
+
+    $.ajax({
+        url: "https://api.edamam.com/search?q=" + dishSearch + "&app_id=" + appID + "&app_key=" + apiKey,
+        method: "GET",
+    })
+    .then(response => {
         console.log(response);
 
     });
+});
