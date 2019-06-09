e735b96e8e26548e1f907666e57f5d5a2789ad29
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-06-09 16:27:48 -0700

    some fixes

--- js/recipe.js
+++ js/recipe.js
@@ -14,7 +14,15 @@ $("#search-button").on("click", function(event){
         method: "GET",
     })
     .then(response => {
+        $("#recipe-view").empty();
         console.log(response);
-
+        for (var i = 0; i < 10; i++)
+        {
+            $("#receipe-view").append("<div id = 'photo'>")
+            //still working on it make each photo it's own container and add text, also make it clickable.
+            $("#recipe-view").append("<img src='" + response.hits[i].recipe.image + "' width = '20%' id = 'image"+i+"'>");
+            $("#image"+i).attr()
+            console.log(response.hits[i].recipe.calories);
+        }
     });
 });
