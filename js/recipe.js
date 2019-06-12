1feba8c3603dbb1c2c1aa8dfc15dcc75f794e31c
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-06-11 19:35:54 -0700

    Update recipe.js

--- js/recipe.js
+++ js/recipe.js
@@ -15,6 +15,22 @@ $("#search-button").on("click", function(event){
     })
     .then(response => {
         console.log(response);
+<<<<<<< Updated upstream
 
+=======
+        for (var i = 0; i < 10; i++)
+        {
+            $("#receipe-view").append("<div id = 'photo'>")
+            //still working on it make each photo it's own container and add text, also make it clickable.
+            $("#recipe-view").append("<img src='" + response.hits[i].recipe.image + " class= 'card-panel hoverable' ' width = '20%' id = 'image"+i+"'>");
+            //$("img").append("<p>Hello World</p>");
+            //$("#recipe-view").append('<div class="card-panel hoverable" width = "20%" id = "image' + i + '> Hoverable Card Panel</div>');
+            //$("#recipe-view").append("<div style='background: url("+response.hits[i].recipe.image+")'  id='image"+i+"'>");
+
+            console.log(response.hits[i].recipe.calories);
+        }
+>>>>>>> Stashed changes
     });
 });
+
+// width='20%'
