aa363a36a51e072e2f3bb7854dde8699b21a0f5f
juney <juhnnie@gmail.com>
2019-06-13 21:23:40 -0700

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 document.getElementById("redirect").onclick = function () {
     location.href = "recipesearch.html";
 };
+
+document.getElementById("logo1").onclick = function () {
+    location.href = "index.html";
+};
